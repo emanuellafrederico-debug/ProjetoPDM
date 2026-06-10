c4014ebf07cebf920d6b76cce26281addf4ef0ef
AlvaroANetto <alababir8@gmail.com>
2026-06-10 14:15:52 -0300

    Mudanças

--- SOD/App.js
+++ SOD/App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>APP EM CONSTRUÇÃO</Text>
+      <Text>APP EM CONSTRUÇÃO POREM COM BRANCH</Text>
       <StatusBar style="auto" />
     </View>
   );
